26739f0723874c1e8f4d00ffa323f90e8bfb1689
Batuhan Belbüken <bbelbuken@hotmail.com>
2024-10-07 22:10:03 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
+import Content from './Content';
+
+const notes = Content();
 
 function App() {
   return (
     <div className="App">
       <Header title='Keeper' />
-      <Note />
+        {notes.map((item) => (
+          <Note 
+            key={item.id}
+            title={item.title}
+            content={item.content}
+          />
+        ))}
       <Footer />
     </div>
   );
