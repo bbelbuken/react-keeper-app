b55af699ef0fdb58b92377fb39eff0bc66a7ead1
Batuhan Belbüken <bbelbuken@hotmail.com>
2024-10-09 22:29:12 +0300

    fresh start

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import Header from './Header';
 import Footer from './Footer';
 import Note from './Note';
-import Content from './Content';
+import CreateArea from './CreateArea';
 
-const notes = Content();
 
 function App() {
   return (
     <div className="App">
       <Header title='Keeper' />
-        {notes.map((item) => (
-          <Note 
-            key={item.id}
-            title={item.title}
-            content={item.content}
-          />
-        ))}
+      <CreateArea />
+      <Note />
       <Footer />
     </div>
   );
