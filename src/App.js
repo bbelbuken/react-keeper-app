8046c15009a37b2cba2666fa280d1c63518fd85f
Batuhan Belbüken <bbelbuken@hotmail.com>
2024-10-09 22:44:36 +0300

    updated states

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import Header from './Header';
-import Footer from './Footer';
-import Note from './Note';
-import CreateArea from './CreateArea';
-
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import CreateArea from "./CreateArea";
+import { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState("")
+  const [content, setContent] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
     <div className="App">
-      <Header title='Keeper' />
-      <CreateArea />
+      <Header title="Keeper" />
+      <CreateArea 
+        title={title} 
+        setTitle={setTitle}
+        content={content} 
+        setContent={setContent}
+        handleSubmit={handleSubmit} 
+      />
       <Note />
       <Footer />
     </div>
--- src/CreateArea.js
+++ src/CreateArea.js
@@ -1,11 +1,23 @@
 import React from "react";
 
-function CreateArea() {
+function CreateArea({ title, setTitle, content, setContent, handleSubmit}) {
   return (
     <div>
-      <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
+      <form onSubmit={handleSubmit}>
+        <input 
+            name="title" 
+            placeholder="Title" 
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            
+        />
+        <textarea 
+            name="content" 
+            placeholder="Take a note..." 
+            rows="3" 
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+        />
         <button>Add</button>
       </form>
     </div>
